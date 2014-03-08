fd61bbd90b776163618ee0de90d492725d003d78
Brandon White <brandontwhite@brandon-whites-macbook-pro.whitehouse>
2014-03-07 18:31:23 -0600

    Initial Commit

---
+++ Pirate Adventure/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Pirate Adventure
+//
+//  Created by Brandon White on 3/7/14.
+//  Copyright (c) 2014 Brandon White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ Pirate Adventure/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pirate Adventure
+//
+//  Created by Brandon White on 3/7/14.
+//  Copyright (c) 2014 Brandon White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
